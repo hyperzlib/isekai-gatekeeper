803e8667ddd83d913577fd62ec857a95ad7337c0
Lex Lim <hyperzlib@outlook.com>
2026-05-06 02:14:03 +0800

    match和matchGlob支持多个表达式

--- src/utils/RuleTools.ts
+++ src/utils/RuleTools.ts
@@ -1,12 +1,20 @@
 import { match } from "path-to-regexp"
 
 export const ruleExpressionTools = {
-  matchGlob: (str: string, pattern: string): boolean => {
-    const matcher = match(pattern, { end: true });
-    return matcher(str) !== false;
+  matchGlob: (str: string, pattern: string | string[]): boolean => {
+    if (Array.isArray(pattern)) {
+      return pattern.some(p => match(p, { end: true })(str) !== false);
+    } else {
+      const matcher = match(pattern, { end: true });
+      return matcher(str) !== false;
+    }
   },
-  match: (str: string, pattern: RegExp): boolean => {
-    return pattern.test(str);
+  match: (str: string, pattern: RegExp | RegExp[]): boolean => {
+    if (Array.isArray(pattern)) {
+      return pattern.some(p => p.test(str));
+    } else {
+      return pattern.test(str);
+    }
   }
 }
 
